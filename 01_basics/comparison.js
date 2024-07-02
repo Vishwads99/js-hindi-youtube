f1d5554ba99793306a45e6a6f1d884c1374458ee
Vishwads99 <vishwadeshpande23@gmail.com>
2024-07-02 15:15:43 +0000

    comparision done

---
+++ 01_basics/comparison.js
@@ -0,0 +1,25 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 <= 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+// console.log(undefined >= 0);
+/* Hence due to confusion we avoid comparing null and undefined*/
+
+//===
+
+console.log("2"==2); // here conversion takes place and 2 is equal to 2
+console.log("2"===2); //here === not only checks the number but also the datatype. 
