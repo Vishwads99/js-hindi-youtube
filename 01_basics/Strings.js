bb3f16540bc92dbe34aaa560a8c58bf5ad601a8c
Vishwads99 <vishwadeshpande23@gmail.com>
2024-07-16 15:03:08 +0000

    Strings Studied

---
+++ 01_basics/Strings.js
@@ -0,0 +1,34 @@
+const name="vishwa" // one way to declare string
+const repoCount="3"
+
+// console.log(name + repoCount + " value"); [not use this syntax]
+
+console.log(`Hello my name is ${name} and my repo Count is ${repoCount}`); // use this syntax
+
+const gameName = new String('vishwa-tnv-com')  // another way to declare string
+
+console.log(gameName[0]); // to access key-value pair
+console.log(gameName[0]);
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('s'));
+
+const newString = gameName.substring(0 , 4) //last value not included also negative value not considered
+console.log(newString);
+
+const anotherString = gameName.slice(-8,4)
+console.log(anotherString);
+
+const newStringOne = "   vishwa   "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://hitesh.com/hitesh%20choudhary"
+console.log(url);
+console.log(url.replace('%20','-'));
+
+console.log(url.includes('hitesh')) //Ask Question
+
+console.log(gameName.split('-')) // Seprates and creates array based on seprator in [' ']
