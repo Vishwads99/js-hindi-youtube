f8978ddef4481d746144c738c2c4a7b7e29ec0b5
Vishwads99 <vishwadeshpande23@gmail.com>
2024-07-16 14:13:03 +0000

    info added

--- 01_basics/datatype-summary.js
+++ 01_basics/datatype-summary.js
@@ -7,7 +7,7 @@
 const id = Symbol('123');
 const anotherid = Symbol('123');
 
-console.log(id === anotherid);
+//console.log(id === anotherid);
 
 // by putting (n) at the end of a big number it becomes bigint datatype.
 const bigNumber= 893493794137491837n
@@ -20,8 +20,33 @@ const bigNumber= 893493794137491837n
 
 const heros = ["Spiderman","Ironman", "Batman"];
 
-let myObj = {name: vishwa, age: 22}   // in curley braces we write key-value Pairs
+let myObj = {name:"vishwa", age:22}   // in curley braces we write key-value Pairs
 
 const myFunction = function(){
-    console.log("Hello World");
+    //console.log("Hello World");
 }
+
+
+// *******************************Types Of Memory********************************
+
+// 1. Stack (Primitive) [here we get copy] , 2. Heap (Non-Primitive) [here we get reference i.e we change directly in original value]
+//1.
+let myYoutubename = "HiteshChoudharydotcom"
+
+let anothername = myYoutubename
+anothername = "chaiaurcode"
+
+console.log(myYoutubename);
+console.log(anothername);
+//2.
+let userOne = {
+    email : "user@google.com",
+    upi : "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "vishwa@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
