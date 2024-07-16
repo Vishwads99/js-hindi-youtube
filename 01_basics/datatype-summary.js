c2d23e7d1baa4dd68eb2b22fc3be699deaa0d5aa
Vishwads99 <vishwadeshpande23@gmail.com>
2024-07-16 13:48:24 +0000

    Datatype Summary

---
+++ 01_basics/datatype-summary.js
@@ -0,0 +1,27 @@
+// Primitive Datatypes (Call by value)
+// 7 Types:- String , Number , Null , Boolean , Undefined , Symbol , Bigint
+
+// JavaScript is Dynamically Typed Language , as we do not need to define type of variable. 
+// like; const score=100 (Dynamic type) and const score:number=100 (static type) {which is used in typescript}
+
+const id = Symbol('123');
+const anotherid = Symbol('123');
+
+console.log(id === anotherid);
+
+// by putting (n) at the end of a big number it becomes bigint datatype.
+const bigNumber= 893493794137491837n
+
+
+
+// Non-Primitive (Call by Reference type)
+
+// Arrays , Objects , Functions 
+
+const heros = ["Spiderman","Ironman", "Batman"];
+
+let myObj = {name: vishwa, age: 22}   // in curley braces we write key-value Pairs
+
+const myFunction = function(){
+    console.log("Hello World");
+}
