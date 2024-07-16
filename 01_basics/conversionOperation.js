7536ac4fcb6235a08d44e82e7e0fd04a4f748b49
Vishwads99 <vishwadeshpande23@gmail.com>
2024-07-16 04:39:05 +0000

    operations done

--- 01_basics/conversionOperation.js
+++ 01_basics/conversionOperation.js
@@ -1,11 +1,11 @@
 let score="33abc"
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber); //this gives (NaN) i.e (not a number) 
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber); //this gives (NaN) i.e (not a number) 
 
 /*
 "33" -> 33;
@@ -18,7 +18,7 @@ null -> 0;
 let isLoggedIn = 1;
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1=> true; 0=> false;
 // "" => false;
@@ -28,5 +28,46 @@ let someNumber=33
 
 let stringNumber= String(someNumber)
 
-console.log(stringNumber);
-console.log(typeof(stringNumber));
+// console.log(stringNumber);
+// console.log(typeof(stringNumber));
+
+
+// **************************Operations*********************
+
+let value=3
+
+let negvalue= -value
+// console.log(negvalue)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3); //raise to the power
+// console.log(2/2);
+// console.log(2%2); // Remainder
+
+let str1= "Hello"
+let str2= " Vishwa"
+
+let str3= str1+str2
+
+// console.log(str3);
+
+// console.log("1"+ 2);
+// console.log(1+"2");
+// console.log("1"+2+2)
+// console.log(1+2+"2")
+
+// console.log(true)
+// console.log(+true) //+ likhanese conversion ho jata hai , kyuki hum operation perform kar rahe hai
+// console.log("")
+// console.log(+"")// + likhanese empty parenthesis ka conversion boolean ho jata hai 
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+//go to mdn and search for further....
+
+
+
+
